55dc59b890933c6f9b292aea5b8254284f53cfc6
pdalinchuk <pavel.dalinchuk@gmail.com>
2022-07-13 14:28:54 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '13d3076c-b1dc-4e1b-afbc-cef129e3d3f9';
+export const MODAL_CLOSE_SEC = 2.5;
